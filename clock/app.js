cf886bfb8952911cb08e728ca0e6e6e0fc83f11f
Leander Leo Lagardo <leander.leo99@gmail.com>
2019-05-08 11:28:32 +0530

    Add files via upload

---
+++ clock/app.js
@@ -0,0 +1,21 @@
+function clock(){
+	const fullDate = new Date();
+	var hour = fullDate.getHours();
+	var min = fullDate.getMinutes();
+	var sec = fullDate.getSeconds();
+	
+	if (hour < 10){
+		hour = "0"+hour;
+	}
+	if (min < 10){
+		min= "0"+min;
+	}
+	if (sec < 10){
+		sec= "0"+sec;
+	}
+	document.getElementById('hour').innerHTML = hour;
+	document.getElementById('min').innerHTML = ": "+min;
+	document.getElementById('sec').innerHTML = ": "+sec;
+}
+
+setInterval(clock,1000);
